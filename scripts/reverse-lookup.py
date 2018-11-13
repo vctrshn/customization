822207d2b7522fe769de628590120948eb23148b
Evan Thomas <evan@tempoautomation.com>
2018-11-12 16:23:12 -0800

    Update 11/12/2018
    Remove Box specific scripts

--- scripts/reverse-lookup.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-import sys
-arg = sys.argv[1] if len(sys.argv) >= 2 else raw_input()
-with open('/Users/ethomas/nova-list', 'r') as f:
-    print(next((line for line in f.readlines() if arg in line), None))
